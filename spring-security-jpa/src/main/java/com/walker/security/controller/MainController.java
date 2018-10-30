cd4f582e73557e54148e468d74c13fb9d99eb4c9
秦苜 <qinmu911@163.com>
2018-10-30 21:19:46 +0800

    feat(jpa): jpa

--- spring-security-jpa/src/main/java/com/walker/security/controller/MainController.java
+++ spring-security-jpa/src/main/java/com/walker/security/controller/MainController.java
@@ -5,8 +5,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
+
     @GetMapping(value = "/login")
     public String login() {
         return "login";
+    }
+
+    @GetMapping("/index")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/403")
+    public String error() {
+        return "403";
     }
 }
