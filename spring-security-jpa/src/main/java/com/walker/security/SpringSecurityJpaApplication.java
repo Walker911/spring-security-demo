be577b3eb2652d866c2d1abec9cf03e820039bd7
walker <qinmu911@163.com>
2019-02-27 16:50:07 +0800

    refactor: use mysql 8

--- spring-security-jpa/src/main/java/com/walker/security/SpringSecurityJpaApplication.java
+++ spring-security-jpa/src/main/java/com/walker/security/SpringSecurityJpaApplication.java
@@ -3,6 +3,9 @@ package com.walker.security;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author walker
+ */
 @SpringBootApplication
 public class SpringSecurityJpaApplication {
 
